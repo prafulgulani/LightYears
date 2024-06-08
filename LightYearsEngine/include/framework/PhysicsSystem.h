0c42bee5ea9e8461d5b4dfd8bc050d89905f44e6
prafulgulani555 <59774145+prafulgulani555@users.noreply.github.com>
2024-06-08 19:33:31 +0530

    lecture 98

---
+++ LightYearsEngine/include/framework/PhysicsSystem.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <box2D/b2_world.h>
+#include "framework/Core.h"
+
+namespace ly
+{
+	class Actor;
+	class PhysicsSystem
+	{
+	public:
+		static PhysicsSystem& Get();
+		void Step(float deltaTime);
+		b2Body* AddListener(Actor* listener);
+		float GetPhysicsScale() const { return mPhysicsScale; }
+	protected:
+		PhysicsSystem();
+
+	private:
+		static unique<PhysicsSystem> physicsSystem;
+		b2World mPhysicsWorld;
+		float mPhysicsScale;
+		int mVelocityIterations;
+		int mPositionIterations;
+	};
+}
---
+++ LightYearsEngine/src/framework/PhysicsSystem.cpp
@@ -0,0 +1,63 @@
+#include <box2d/b2_body.h>
+#include <box2d/b2_fixture.h>
+#include <box2d/b2_polygon_shape.h>
+
+#include "framework/PhysicsSystem.h"
+#include "framework/Actor.h"
+#include "framework/MathUtility.h"
+
+
+namespace ly
+{
+	unique<PhysicsSystem> PhysicsSystem::physicsSystem{ nullptr };
+	PhysicsSystem& PhysicsSystem::Get()
+	{
+		if (!physicsSystem)
+		{
+			physicsSystem = std::move(unique<PhysicsSystem>{new PhysicsSystem});
+		}
+		return *physicsSystem;
+	}
+
+	void PhysicsSystem::Step(float deltaTime)
+	{
+		mPhysicsWorld.Step(deltaTime, mVelocityIterations, mPositionIterations);
+	}
+
+	b2Body* PhysicsSystem::AddListener(Actor* listener)
+	{
+		if (listener->isPendingDestroy()) return nullptr;
+
+		b2BodyDef bodyDef;
+		bodyDef.type = b2_dynamicBody;
+		
+		bodyDef.userData.pointer = reinterpret_cast<uintptr_t>(listener);
+		
+		bodyDef.position.Set(listener->GetActorLocation().x * GetPhysicsScale(), listener->GetActorLocation().y * GetPhysicsScale());
+		bodyDef.angle = DegreesToRadians(listener->GetActorRotation());
+	
+		b2Body* body = mPhysicsWorld.CreateBody(&bodyDef);
+
+		b2PolygonShape shape;
+		auto bound = listener->GetActorGloabalBounds();
+		shape.SetAsBox(bound.width/2.f * GetPhysicsScale(), bound.height/2.f * GetPhysicsScale());
+		
+		b2FixtureDef fixtureDef;
+		fixtureDef.shape = &shape;
+		fixtureDef.density = 1.0f;
+		fixtureDef.friction = 0.3f;
+		fixtureDef.isSensor = true;
+
+		body->CreateFixture(&fixtureDef);
+	
+		return body;
+	}
+
+	PhysicsSystem::PhysicsSystem()
+		: mPhysicsWorld{ b2Vec2{0.f, 0.f} },
+		mPhysicsScale{0.01f},
+		mVelocityIterations{8},
+		mPositionIterations{3}
+	{
+	}
+}
