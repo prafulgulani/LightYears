30f07f1176ef4aa5eac689dbfe27b6811cf54ebc
prafulgulani555 <59774145+prafulgulani555@users.noreply.github.com>
2024-08-11 14:19:21 +0530

    169

--- LightYearsEngine/include/framework/BackdropActor.h
+++ LightYearsEngine/include/framework/BackdropActor.h
@@ -6,7 +6,7 @@ namespace ly
 	class BackdropActor : public Actor
 	{
 	public:
-		BackdropActor(World* owningWorld, const std::string& texturePath, const sf::Vector2f& velocity = sf::Vector2f{0.f, 50.f});
+		BackdropActor(World* owningWorld, const std::string& texturePath, const sf::Vector2f& velocity = sf::Vector2f{0.f, -50.f});
 		virtual void Tick(float deltaTime) override;
 	private:
 		sf::Vector2f mMovingVelocity;
